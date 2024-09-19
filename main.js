546a8942f1c8eac86d9a3e08f6b825ffebc52292
Crawfish2 <umakawata@gmail.com>
2024-09-19 05:18:32 +0000

    9-19 JavaScriptの導入

---
+++ main.js
@@ -0,0 +1,17 @@
+document.querySelector("h1").textContent="河田"
+document.querySelector("h2").textContent="悠慎"
+
+//documentとは！！
+/*htmlファイルで書かれているコードを一つのオブジェクトとしてみなす。*/
+
+//DOM(ドム)
+/*ドキュメントオブジェクトモデル（Document Object Model）の略で、webページの要素やコンテンツなどをツリー構造で表現したデータモデル */
+
+//上記コードの意味
+/* document.querySelector("h1").textContent="自分の名前"*/
+
+//ドキュメントの中にある"h1"タグを探してね
+//document.querySelector("h1")
+
+//h1タグの中身のテキストを変えてね
+//.textContent="自分の名前"
