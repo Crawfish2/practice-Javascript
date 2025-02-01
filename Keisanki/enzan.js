953252ba8cd6d44b2548bc89ff8ff8b9158efdc2
Crawfish2 <umakawata@gmail.com>
2025-02-01 03:20:36 +0000

    計算機

---
+++ Keisanki/enzan.js
@@ -0,0 +1,15 @@
+const enzan = document.getElementById("kakubasyo");
+let answer = 0;
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eite = document.getElementById("eite");
+const nine = document.getElementById("nine");
+const zero = document.getElementById("zero");
+one.addEventListener("click",function(){
+    enzan.textContent = "1"
+});
