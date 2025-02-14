36470250979cff766829ab34a1c2c57689edd55b
Crawfish2 <umakawata@gmail.com>
2025-02-14 05:43:04 +0000

    switch文書いた

--- Keisanki/enzan.js
+++ Keisanki/enzan.js
@@ -2,16 +2,32 @@ function enzan() {
     const enzansi = document.getElementsByClassName("enzansi");
     const btn = document.getElementsByClassName("suuzi");
     const kakikomi = document.getElementById("kakubasyo");
-    let keisan = ;
-    if (enzansi == "="){
-        eval();
-        kakikomi.textContent = "";
-    }
-    else if (enzansi == "reset"){
-        kakikomi.textContent = "";
-    }
-    else if (enzansi == "+"){
-        kakikomi.textContent = "+";
+    let keisan = 0;
+    switch (enzansi){
+        case "=":
+            eval(keisan);
+            kakikomi.textContent = keisan;
+            break;
 
+        case "reset":
+            kakikomi.textContent = "";
+            keisan = 0;
+            break;
+
+        case "+":
+            if (keisan != 0 ) {
+                kakikomi.textContent = kakikomi + "+";
+                keisan = keisan + "+";
+            }
+            break;
+
+        case "-":
+            if (keisan != 0 ) {
+                kakikomi.textContent = kakikomi + "-";
+                keisan = keisan + "-";
+            }
+            break;
+        default:
+            kakikomi.textContent = "Eroor";
     }
 }
