a76621bb9817380a42e43237e7db58b4f5eb809d
Crawfish2 <umakawata@gmail.com>
2025-02-24 09:10:30 +0000

    計算機完成

--- calculator/enzan.js
+++ calculator/enzan.js
@@ -1,52 +1,100 @@
 function enzan(nyuryoku) {
     const kakikomi = document.getElementById("kakubasyo");
-    let keisan = 0;
-    
     switch (nyuryoku){
+        case 0:
+            kakikomi.value += "0";
+            break;
+
         case 1:
-            kakikomi.textContent = "1";
-            keisan = keisan + 1
+            kakikomi.value += "1";
             break;
 
-        case 10:
-            // if (keisan != 0 ) {
-                kakikomi.textContent = "+";
-                // keisan = keisan + "+";
-            //}
+        case 2:
+            kakikomi.value += "2";
             break;
 
-        case 11:
-            if (keisan != 0 ) {
-                kakikomi.textContent = kakikomi + "-";
-                keisan = keisan + "-";
+        case 3:
+            kakikomi.value += "3";
+            break;
+
+        case 4:
+            kakikomi.value += "4";
+            break;
+
+        case 4:
+            kakikomi.value += "4";
+            break;
+
+        case 5:
+            kakikomi.value += "5";
+            break;
+
+        case 6:
+            kakikomi.value += "6";
+            break;
+
+        case 7:
+            kakikomi.value += "7";
+            break;
+
+        case 8:
+            kakikomi.value += "8";
+            break;
+
+        case 9:
+            kakikomi.value += "9";
+            break;
+
+        case 10://加算
+            if(kakikomi.value != ""){
+                kakikomi.value += "+";
             }
             break;
 
-            case 12:
-            if (keisan != 0 ) {
-                kakikomi.textContent = kakikomi + "×";
-                keisan = keisan + "*";
+        case 11://減算
+            if(kakikomi.value != ""){
+                kakikomi.value += "-";
             }
             break;
 
-            case 13:
-            if (keisan != 0 ) {
-                kakikomi.textContent = kakikomi + "÷";
-                keisan = keisan + "/";
+        case 12://乗算
+            if(kakikomi.value != ""){
+                kakikomi.value += "*";
             }
             break;
 
-        case 14:
-            eval(keisan);
-            kakikomi.textContent = keisan;
+        case 13://除算
+            if(kakikomi.value != ""){
+                kakikomi.value += "/";
+            }
             break;
 
-        case 15:
-            kakikomi.textContent = "";
-            keisan = 0;
+        case 14://イコール
+            kakikomi.value = eval(kakikomi.value);
+            break;
+
+        case 15://リセット
+            kakikomi.value = "";
             break;
 
         default:
-            kakikomi.textContent = "Eroor";
+            kakikomi.value = "Error";
     }
 }
+
+// function enzan(btn) {
+//     if(btn.value == "=") {
+//       document.getElementById("kakubasyo").value = eval(document.getElementById("kakubasyo").value);
+//     } else if (btn.value == "C") {
+//       document.getElementById("kakubasyo").value = "";
+//     } else {
+//       if (btn.value == "×") {
+//         btn.value = "*";
+//       } else if (btn.value == "÷") {
+//         btn.value = "/";
+//       } 
+//       document.getElementById("kakubasyo").value += btn.value;
+//       document.getElementById("kakubasyo").value = "×";
+//       document.getElementById("kakubasyo").value = "÷";
+//     }
+//   }
